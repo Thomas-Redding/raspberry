2a696319d9ffc6022cfb093808ac0b35e6f546e8
Thomas Redding <thomasredding@Thomass-MacBook-Pro-3.local>
2019-10-13 21:55:13 -0700

    nit

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
   "net/http"
 )
 
-func bucketRoot() string { return "/Users/John/files/to/serve" }
+func bucketRoot() string {
+  return "/Users/John/files/to/serve"
+}
 
 func start() {}
 
 func handle(writer http.ResponseWriter, request *http.Request, system *System) {
-	system.FileServe(request)
+  system.FileServe(request)
 }
